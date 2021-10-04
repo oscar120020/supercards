e5ff21c700644ad4ef475be119102556ce6bee30
oscar120020 <omartinez@intellisys.com.do>
2021-10-04 16:26:55 -0400

    klk

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
 
-mongoose.connect("mongodb+srv://oscar12002:<oscar12002>@cluster0.qtit0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://oscar12002:oscar12002@cluster0.qtit0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     .then(db => console.log("DB is connected"))
     .catch(err => console.log(err))
