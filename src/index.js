0fb0e1ae4a8ac408068ce27bf87e2c8d13e50347
oscar120020 <omartinez@intellisys.com.do>
2021-10-04 14:42:36 -0400

    123

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const PORT = process.env.PORT ||  3500
+const PORT = process.env.PORT ||  3000
 
 app.listen(PORT, (req, res) => {
     console.log("Serve running on port 3500");
