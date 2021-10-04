fceb75662a8c90ac1b83b4ca4e9fa054392941c9
oscar120020 <omartinez@intellisys.com.do>
2021-10-04 15:23:30 -0400

    mlab

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/supercards")
+mongoose.connect("mongodb+srv://oscar12002:<oscar12002>@cluster0.qtit0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     .then(db => console.log("DB is connected"))
     .catch(err => console.log(err))
