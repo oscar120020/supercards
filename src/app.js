0f4b6001af6d8cf1bd06ece149893fc474fb6ca6
oscar120020 <omartinez@intellisys.com.do>
2021-10-04 15:36:41 -0400

    a ver

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.use(urlencoded({extended: false}))
 app.use(morgan('dev'))
 
 // Routes
+app.get("/", (req, res) => {
+    res.json({mss: "llllllll"})
+})
 app.use('/api/supercards', require('./routes/superCards.route').default)
 
 export default app
