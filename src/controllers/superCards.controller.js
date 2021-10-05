0ca02ed57a0fffe8e3bba32fe91c4ba4e94da168
oscar120020 <omartinez@intellisys.com.do>
2021-10-05 12:37:52 -0400

    verificaciones

--- src/controllers/superCards.controller.js
+++ src/controllers/superCards.controller.js
@@ -9,20 +9,29 @@ const getSuperCards = async (req, res) => {
 const createSuperCards = async (req, res) => {
     const { name, universe, image } = req.body
 
-    const newCard = new SuperCard({
-        name,
-        universe,
-        image
-    })
+    if (name && universe && image){
+        const newCard = new SuperCard({
+            name,
+            universe,
+            image
+        })
+    
+        await newCard.save()
+        res.status(201).json({message: "card created successfully"})
+    }else{
+        res.status(400).json({message: "name, universe and image are required"})
+    }
 
-    await newCard.save()
-    res.status(201).json({message: "card created successfully"})
 }
 
 const updateSuperCards = async (req, res) => {
     const id = req.params.cardId
-    await SuperCard.findByIdAndUpdate(id, req.body)
-    res.status(200).json({message: `Card with id ${id} has been updated`})
+    if(Object.keys(req.body).length !== 0){
+        await SuperCard.findByIdAndUpdate(id, req.body)
+        res.status(200).json({message: `Card with id ${id} has been updated`})
+    }else{
+        res.status(400).json({message: `Nothing to update`})
+    }
 }
 
 const deleteSuperCards = async (req, res) => {
