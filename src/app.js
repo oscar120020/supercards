47ed7e88cdd4aea98c1ab3b29a03986df176c6b6
oscar120020 <omartinez@intellisys.com.do>
2021-10-05 15:37:26 -0400

    cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express, { json, urlencoded } from 'express'
 import './database'
+import cors from 'cors'
 
 import cardsRouter from './routes/superCards.route'
 
 const app = express()
 
 // Middelweres
+app.use(cors())
 app.use(json())
 app.use(urlencoded({extended: false}))
 
