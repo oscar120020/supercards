20c3d7704ead8231e3a2ccb39dc9200732a91ccf
oscar120020 <omartinez@intellisys.com.do>
2021-10-11 11:06:47 -0400

    add limit

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 import express, { json, urlencoded } from 'express'
 import './database'
 import cors from 'cors'
-
 import cardsRouter from './routes/superCards.route'
 
 const app = express()
 
 // Middelweres
 app.use(cors())
-app.use(json())
+app.use(json({limit: '5mb'}))
 app.use(urlencoded({extended: false}))
 
 // Routes
-
 app.use('/api/supercards', cardsRouter)
 
 export default app
